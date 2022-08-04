081726f709dc809b8d5d8ca3e10004ea8a606945
jihennasri <jihene.nassri@gmail.com>
2022-08-04 23:00:28 +0200

    factoriel using for and while loop

--- Main.java
+++ Main.java
@@ -4,16 +4,24 @@ import java.util.Scanner; // import the Scanner class
 class Main {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-    int n=0;
+    int n =0 ,s=1 ;
    
     
     // Enter number to test
-    System.out.println("donner n pour calculer Un"); 
+    System.out.println("donner n pour calculer"); 
     n = input.nextInt();
-    int U=2;
-      for (int i=0; i <= n; i++) 
-            U=3*(U)+4;
-     
+    
+      /*for (int i=n; i>=1; i--) 
+          {
+            s=s*i;} 
+
+     System.out.println("factoriel= "+s); */
+     int i=n;
+     while (i>=1){
+      s=s*i;
+      i--;
+     }
+     System.out.println("factoriel of the number  = "+s);
 
         
        
@@ -21,6 +29,6 @@ class Main {
     
     
 
-  } 
+   
     
-}
+}}
