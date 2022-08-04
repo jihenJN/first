817561cb3581ef3dc5caf62eb8b3420a62da1907
jihennasri <jihene.nassri@gmail.com>
2022-08-04 12:06:24 +0200

    la somme avec while

--- Main.java
+++ Main.java
@@ -4,39 +4,20 @@ import java.util.Scanner; // import the Scanner class
 class Main {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-    int a,b,c;
-    double delta , x1, x2;
+    int number=0, somme=0;
+   
     
-    // Enter a,b,c 
-    System.out.println("Enter a value"); 
-    a = input.nextInt();    
-    System.out.println("value a  is: " + a);  
-    System.out.println("Enter b value"); 
-    b = input.nextInt();    
-    System.out.println("value b  is: " + b);
-    c = input.nextInt();    
-    System.out.println("value b  is: " + c);   
+    // Enter number to test
+    System.out.println("Enter a number to test"); 
+    number = input.nextInt();
+    int i = 0;
+      while (i <= number) {
+       somme=somme+i ;
+       i++;
+    } 
+    System.out.println("somme = "+somme); 
+    
 
-    //calculate delta 
-    delta = (b*b) - (4*a*c) ;
-
-    // test delta value 
-    if (delta > 0){
-
-      x1 =(-b-Math.sqrt(delta )) /(2*a);
-      x2= (-b+Math.sqrt(delta ))/(2*a);
-      System.out.println("x1="+x1);
-      System.out.println("x2="+x2);
-
-    }else if(delta == 0){
-      x1= -b /(2*a);
-      System.out.println("x1=x2="+x1);
-     
-  }
-  else {
-
-    System.out.println("pas de racine");
-  }
-
-  }
+  } 
+    
 }
