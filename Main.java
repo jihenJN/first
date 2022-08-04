233df6ec66faeb24483c781b87e006a6823b9009
jihennasri <jihene.nassri@gmail.com>
2022-08-04 12:56:53 +0200

    factoriel boucle for et while

--- Main.java
+++ Main.java
@@ -4,13 +4,13 @@ import java.util.Scanner; // import the Scanner class
 class Main {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-    int n=0,;
+    int n=0;
    
     
     // Enter number to test
     System.out.println("donner n pour calculer Un"); 
     n = input.nextInt();
-    int U;
+    int U=2;
       for (int i=0; i <= n; i++) 
             U=3*(U)+4;
      
