6df96e0e33d0192e391db78ae94898be9145ff71
jihennasri <jihene.nassri@gmail.com>
2022-08-07 09:50:15 +0200

    Table creation & sum

--- Main.java
+++ Main.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 public class Main {
    public static void main(String[] args) {
     Scanner input = new Scanner (System.in);
-    int lengthTable,Sum =0;
+    int lengthTable =0,Sum =0;
     int [] T={};
     // enter length of table
     System.out.println("enter the length of the table");
     lengthTable = input.nextInt();
+   
     // enter values of table 
     System.out.println("enter the values of the table");
      for (int i=0; i< T.length ;i++){
