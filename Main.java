b81a1197c1b3be233f4774433f264801e13c524d
jihennasri <jihene.nassri@gmail.com>
2022-08-07 09:44:28 +0200

    Table creation & sum

--- Main.java
+++ Main.java
@@ -1,27 +1,35 @@
+import java.util.Scanner;
 
-import java.util.Scanner; // import the Scanner class 
+public class Main {
+   public static void main(String[] args) {
+    Scanner input = new Scanner (System.in);
+    int lengthTable,Sum =0;
+    int [] T={};
+    // enter length of table
+    System.out.println("enter the length of the table");
+    lengthTable = input.nextInt();
+    // enter values of table 
+    System.out.println("enter the values of the table");
+     for (int i=0; i< T.length ;i++){
+        T[i]=input.nextInt();
+        }
+     // calculate the Sum of table 
+     for (int i=0 ;i< T.length ;i++){
+        Sum = Sum + T[i];
+     }
+     System.out.println("Sum values of the table = " + Sum);
+     //
+    
 
-class Main {
-  public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    int n =0 ,s=1 ;
+    
+
+
+    
+ 
+    
    
     
-    // Enter number to test
-    System.out.println("donner n pour calculer"); 
-    n = input.nextInt();
-    
-      /*for (int i=n; i>=1; i--) 
-          {
-            s=s*i;} 
-
-     System.out.println("factoriel= "+s); */
-     int i=n;
-     while (i>=1){
-      s=s*i;
-      i--;
-     }
-     System.out.println("factoriel of the number  = "+s);
+   
 
         
        
